ec02bb8b41ebe456b405384e1af4e910c2f8b5b3
diwangkeji <168322853+diwangkeji@users.noreply.github.com>
2025-01-05 21:42:26 +0800

    Update useAccessPrompt.ts

--- src/hooks/useAccessPrompt.ts
+++ src/hooks/useAccessPrompt.ts
@@ -3,7 +3,7 @@ export const useAccessPrompt = () => {
   setTimeout(() => {
     window['$notification'].create({
       title: '友情提示',
-      content: '此站点仅供演示、学习所用，请勿进行非法操作、上传或发布违法资讯。',
+      content: '本站仅用于学习、交流之用，请勿进行非法操作、上传或发布违法资讯。',
       duration: 30000
     })
   }, 3000)
